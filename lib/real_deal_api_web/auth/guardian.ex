1ceeba4ed1c1566378cbc6b63e9136b757acc480
oscar <oscar.burman005@gmail.com>
2025-11-02 12:43:52 +0100

    Set up basic auth

--- lib/real_deal_api_web/auth/guardian.ex
+++ lib/real_deal_api_web/auth/guardian.ex
@@ -21,4 +21,26 @@ defmodule RealDealApiWeb.Auth.Guardian do
   def resource_form_claims(_claims) do
     {:error, :no_id_provided}
   end
+
+  def authenticate(email, password) do
+    case Accounts.get_account_by_email(email) do
+      nil ->
+        {:error, :unauthorized}
+
+      account ->
+        case validate_password(password, account.hashed_password) do
+          true -> create_token(account)
+          false -> {:error, :unauthorized}
+        end
+    end
+  end
+
+  defp validate_password(password, hashed_password) do
+    Pbkdf2.verify_pass(password, hashed_password)
+  end
+
+  defp create_token(account) do
+    {:ok, token, _claims} = encode_and_sign(account)
+    {:ok, account, token}
+  end
 end

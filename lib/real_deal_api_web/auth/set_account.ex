22281592e26e81dda8ee0b81011943266a3e8649
oscar <oscar.burman005@gmail.com>
2025-11-03 12:49:49 +0100

    Update user object

--- lib/real_deal_api_web/auth/set_account.ex
+++ lib/real_deal_api_web/auth/set_account.ex
@@ -1,17 +1,26 @@
 defmodule RealDealApiWeb.Auth.SetAccount do
   import Plug.Conn
+  alias RealDealApi.Accounts
   alias RealDealApiWeb.Auth.ErrorResponse
 
   def init(_options) do
   end
 
   def call(conn, _opts) do
-    account = Guardian.Plug.current_resource(conn)
+    # Only assign if :account not already assigned
+    case conn.assigns[:account] do
+      nil ->
+        account_id = get_session(conn, :account_id)
 
-    if account do
-      assign(conn, :account, account)
-    else
-      raise ErrorResponse.Unauthorized
+        if account_id == nil, do: raise(ErrorResponse.Unauthorized)
+
+        case Accounts.get_full_account(account_id) do
+          nil -> raise(ErrorResponse.Unauthorized)
+          account -> assign(conn, :account, account)
+        end
+
+      _account ->
+        conn
     end
   end
 end
